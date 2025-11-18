d6612174a4b0bccc17597707b72ff78669a53304
Eariele Arango <earielearango@gmail.com>
2025-11-18 10:13:55 +0400

    changes to js

--- sticky-navigation/script.js
+++ sticky-navigation/script.js
@@ -3,8 +3,8 @@ window.addEventListener'scroll', fixNav)
 
 function fixNa() {
     if(window.scrollY > nav.offsetHeight + 150) {
-        nav.classList.add('active')
+        nav.classList.add('activ')
      else {
         nav.classList.remove('active')
     }
-}
+
